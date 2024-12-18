d107d3af2927d2da3852317d5179969fc8f52423
Tang Ziya <tcztzy@gmail.Com>
2024-12-18 23:42:13 +0800

    Fixed: setup
    1. fix setup process
    2. update README

--- hatch_build.py
+++ hatch_build.py
@@ -7,25 +7,62 @@ from sys import stderr
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface  # type: ignore
 
 
-class NpmBuildHook(BuildHookInterface):
-    def initialize(self, version, build_data):
-        def copy(src: str, dst: str):
-            if not (os.path.exists(dst) and os.path.isfile(dst)):
-                shutil.copy2(src, dst)
+def _(name: str):
+    e = shutil.which(name)
+    if e is None:
+        raise RuntimeError(
+            f"`{name}` is required for building Omni Webui but it was not found"
+        )
+    return e
 
+
+uv = _("uv")
+git = _("git")
+deno = _("deno")
+_("npm")
+
+
+def is_ignored(path: str) -> bool:
+    rules = [
+        rule[2:]
+        for rule in (Path(__file__).parent / ".gitignore").read_text().splitlines()
+        if rule.strip() and rule.startswith("!/src")
+    ]
+    rpath = Path(path).relative_to(Path.cwd())
+    return rpath not in rules
+
+
+def copy(src: str, dst: str):
+    if is_ignored(dst):
+        shutil.copy2(src, dst)
+
+
+class DenoBuildHook(BuildHookInterface):
+    def initialize(self, version, build_data):
         super().initialize(version, build_data)
         print(">>> Building Omni Webui frontend\n", file=stderr)
-        uv = shutil.which("uv")
-        if uv is None:
-            raise RuntimeError(
-                "`uv` is required for building Omni Webui but it was not found"
-            )
+
         cache_dir = Path(
             subprocess.check_output([uv, "--color", "never", "cache", "dir"])
             .decode()
             .strip()
         )
         webui_dir = cache_dir.joinpath("git-v0/checkouts/d6dc8c520e52253d/29a271959")
+        if not webui_dir.exists():
+            # TODO: currently solution is so dirty, need to find a better way to clone the repo
+            subprocess.run(
+                [
+                    git,
+                    "clone",
+                    "--depth",
+                    "1",
+                    "--branch",
+                    "v0.4.8",
+                    "https://github.com/open-webui/open-webui",
+                    webui_dir,
+                ],
+                check=True,
+            )
         build_dir = Path.cwd()
         directories = [
             "src",
@@ -44,7 +81,6 @@ class NpmBuildHook(BuildHookInterface):
         ]
         for f in files:
             shutil.copy2(webui_dir / f, build_dir / f)
-        deno = shutil.which("deno")
         if deno is None:
             raise RuntimeError(
                 "`deno` is required for building Omni Webui but it was not found"
