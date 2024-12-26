3d855ef6d9e400f37b21e4fe2f43702b72e2bf5a
alexlee <344322703@qq.com>
2024-12-26 16:59:24 +0800

    Added: upload file and show user file functions
    1. Added file entrypoints

--- omni_webui/routes/api/v1/__init__.py
+++ omni_webui/routes/api/v1/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from .users import router as users_router
+from .files import router as file_router
 
 router = APIRouter()
 
 router.include_router(users_router, prefix="/users")
+router.include_router(file_router, prefix="/files")
