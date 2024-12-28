27998b8f68fc63203710e94f0052469c7d9dd6bb
Tang Ziya <tcztzy@gmail.Com>
2024-12-28 18:27:47 +0800

    Added: chat component
    1. Added chat component and openai api entrypoint

--- omni_webui/routes/api/v1/__init__.py
+++ omni_webui/routes/api/v1/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from .files import router as files_router
+from .openai import router as openai_router
 from .users import router as users_router
 
 router = APIRouter()
 
 router.include_router(users_router, prefix="/users")
 router.include_router(files_router, prefix="/files")
+router.include_router(openai_router, prefix="/openai")
