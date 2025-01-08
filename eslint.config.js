548c4e9f40f7c4577cc97c996078ba364d7e815e
Tang Ziya <tcztzy@gmail.Com>
2025-01-08 21:37:44 +0800

    Added: eslint-plugin-svelte
    Added eslint-plugin-svelte

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
+import eslintPluginSvelte from "eslint-plugin-svelte";
+import globals from "globals";
 import tseslint from "typescript-eslint";
-
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+	{ files: ["**/*.{js,mjs,cjs,ts}"] },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	...eslintPluginSvelte.configs["flat/recommended"],
 ];
