3df23692b1d0eab1bf41d9782e489dc59328128e
Tang Ziya <tcztzy@gmail.Com>
2025-01-08 13:26:35 +0800

    Removed: peewee-migrate
    Ridiculous peewee & open-webui

--- open_webui/internal/db.py
+++ open_webui/internal/db.py
@@ -1,77 +1,57 @@
+"""Internal database module for the Open Web UI."""
+
 import json
 import logging
 from contextlib import contextmanager
 from typing import Any, Optional
 
-from open_webui.internal.wrappers import register_connection
+from sqlalchemy import Dialect, create_engine, types
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.pool import NullPool, QueuePool
+from sqlalchemy.sql.type_api import _T
+from typing_extensions import Self
+
 from open_webui.env import (
-    OPEN_WEBUI_DIR,
-    DATABASE_URL,
-    SRC_LOG_LEVELS,
     DATABASE_POOL_MAX_OVERFLOW,
     DATABASE_POOL_RECYCLE,
     DATABASE_POOL_SIZE,
     DATABASE_POOL_TIMEOUT,
+    DATABASE_URL,
+    SRC_LOG_LEVELS,
 )
-from peewee_migrate import Router
-from sqlalchemy import Dialect, create_engine, types
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.pool import QueuePool, NullPool
-from sqlalchemy.sql.type_api import _T
-from typing_extensions import Self
 
 log = logging.getLogger(__name__)
 log.setLevel(SRC_LOG_LEVELS["DB"])
 
 
 class JSONField(types.TypeDecorator):
+    """Ridiculous!!!."""
+
     impl = types.Text
     cache_ok = True
 
     def process_bind_param(self, value: Optional[_T], dialect: Dialect) -> Any:
+        """Ridiculous!!!."""
         return json.dumps(value)
 
     def process_result_value(self, value: Optional[_T], dialect: Dialect) -> Any:
+        """Ridiculous!!!."""
         if value is not None:
             return json.loads(value)
 
     def copy(self, **kw: Any) -> Self:
-        return JSONField(self.impl.length)
+        """Ridiculous!!!."""
+        return JSONField(self.impl.length)  # type: ignore
 
     def db_value(self, value):
+        """Ridiculous!!!."""
         return json.dumps(value)
 
     def python_value(self, value):
+        """Ridiculous!!!."""
         if value is not None:
             return json.loads(value)
-
-
-# Workaround to handle the peewee migration
-# This is required to ensure the peewee migration is handled before the alembic migration
-def handle_peewee_migration(DATABASE_URL):
-    # db = None
-    try:
-        # Replace the postgresql:// with postgres:// to handle the peewee migration
-        db = register_connection(DATABASE_URL.replace("postgresql://", "postgres://"))
-        migrate_dir = OPEN_WEBUI_DIR / "internal" / "migrations"
-        router = Router(db, logger=log, migrate_dir=migrate_dir)
-        router.run()
-        db.close()
-
-    except Exception as e:
-        log.error(f"Failed to initialize the database connection: {e}")
-        raise
-    finally:
-        # Properly closing the database connection
-        if db and not db.is_closed():
-            db.close()
-
-        # Assert if db connection has been closed
-        assert db.is_closed(), "Database connection is still open."
-
-
-handle_peewee_migration(DATABASE_URL)
 
 
 SQLALCHEMY_DATABASE_URL = DATABASE_URL
@@ -104,6 +84,7 @@ Session = scoped_session(SessionLocal)
 
 
 def get_session():
+    """Ridiculous!!!."""
     db = SessionLocal()
     try:
         yield db
